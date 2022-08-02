25dafecfc63e408c82cb82594efbfaedc4bced0b
VaskoMishael <mike.drmr03@gmail.com>
2022-08-02 22:05:17 +0200

    Divided to components stucture, same functionality, no style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoDataService } from './todo-data.service';
 import { FormsModule } from '@angular/forms';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoListHeaderComponent, TodoListComponent, TodoListItemComponent, TodoListFooterComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [TodoDataService],
   bootstrap: [AppComponent],
